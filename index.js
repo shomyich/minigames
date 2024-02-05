a463d9dcc22e1a7db9bb2c87272c79a3d5871be7
shomyich <220009@astanait.edu.kz>
2024-02-05 18:41:39 +0600

    frontend for auth

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const authRouter = require('./authRouter')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Import cors
 
-const app = express()
+const authRouter = require('./authRouter');
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use("/auth", authRouter)
+const app = express();
+
+app.use(cors()); // Use cors middleware
+app.use(express.json());
+app.use('/auth', authRouter);
+
 const start = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://raviollll9874:ATBKVuVjzVGZQTFX@gamedb.0edk99n.mongodb.net/?retryWrites=true&w=majority`)
-        app.listen(PORT, () => console.log(`server started on port ${PORT}`))
-    } catch(e){
-        console.log(e)
+        await mongoose.connect(
+            'mongodb+srv://raviollll9874:ATBKVuVjzVGZQTFX@gamedb.0edk99n.mongodb.net/?retryWrites=true&w=majority'
+        );
+        app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
     }
-}
+};
 
-
-start()
+start();
